e798f3004b1640f67ee3c7a3e168f3c8cfe2a49e
Admin <brycealvir@gmail.com>
2022-10-19 20:39:51 -0700

    added tsparticles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from './components/Particles'
+import React from "react";
+
 
 function App() {
+ 
   return (
-    <Navbar />
+    <>
+      <Particles />
+      <Navbar />
+      <Header />
+    </>
+    
   );
 }
 
---
+++ src/components/Particles.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Particle from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+
+const Particles = () => {
+  const particleInit = async (main) => await loadFull(main)
+  const particlesOptions = {
+    particles: {
+      color: {
+        value: '#01EFD1',
+      },
+
+      links: {
+        color: '#01EFD1',
+        distance: 130,
+        enable: true,
+        opacity: 0.5,
+        width: 0.5,
+      },
+      move: {
+        direction: 'none',
+        enable: true,
+        outModes: {
+          default: 'bounce',
+        },
+        random: true,
+        speed: 5,
+        straight: true,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 900,
+        },
+        value: 30,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 2,
+          color: '#01EFD1',
+        },
+      },
+      size: {
+        anim: {
+          enable: true,
+          size_min: 0.1,
+          speed: 10,
+        },
+      },
+    },
+  }
+
+  return <Particle init={particleInit} options={particlesOptions} />
+}
+
+export default Particles
