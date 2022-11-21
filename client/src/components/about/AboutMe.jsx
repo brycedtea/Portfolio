e29d8832be86f2f0b60283e88c49b0a78da7ae04
Admin <brycealvir@gmail.com>
2022-11-20 18:31:12 -0800

    edited about me

--- client/src/components/about/AboutMe.jsx
+++ client/src/components/about/AboutMe.jsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
         <div className='col-lg-6 col-xm-12'>
           <h2 className='text-uppercase about-heading'>About Me</h2>
           <p className='about-info'>
-          Hello, I am Bryce. I am a dedicated IT professional with a passion for programming. I recently worked as a Level 2 IT Support Engineer for an autonomous car company while also attending a Full Stack Software Development Bootcamp in the evenings. I possess over 3 years of IT support experience and 5 years of customer service experience. My technical knowledge combined with my strong interpersonal skills have allowed me to adapt to any situation. I hope to leverage these skills and provide you with quality service.
+          Hello, I am Bryce. I am a dedicated IT professional with a passion for programming. I recently worked as a Level 2 IT Support Engineer for an autonomous car company while also attending a Full Stack Software Development Bootcamp in the evenings. I possess over 3 years of IT support experience and 5 years of customer service experience. I am recognized for my ability to work in fast-paced environments, as well as my ability to collaborate effectively with a team. Additionally, I have a proven track record in developing innovative solutions to complex problems. I am always seeking out opportunities to maximize profitability and performance. I hope to leverage these skills to make an immediate contribution to your team.
           </p>
         </div>
       </div>
