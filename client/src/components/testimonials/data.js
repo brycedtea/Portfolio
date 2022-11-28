155afc3cd86cd063b4e39ff70631a6f888d8d801
Admin <brycealvir@gmail.com>
2022-11-28 14:11:24 -0800

    added testimonial

--- client/src/components/testimonials/data.js
+++ client/src/components/testimonials/data.js
@@ -6,34 +6,35 @@ import avatar4 from './avatars/avatar-4.png'
 import dev from './avatars/dev.jpg'
 import kris from './avatars/kris.jpg'
 import dan from './avatars/dan.jpg'
+import kristan from './avatars/kristan.jpg'
 
 export const testimonialsInfo = [
   {
-    img: avatar1,
-    name: 'John Doe 1',
-    title: 'Colleague',
+    img: dev,
+    name: 'Devenn Viray',
+    title: 'IT Support Specialist at The Linde Group',
     review:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione sequi saepe dicta blanditiis, molestias eum excepturi quod fugiat veniam!',
   },
   {
-    img: avatar2,
-    name: 'Jane Doe 2',
-    title: 'Colleague',
+    img: kris,
+    name: 'Kris Huynh',
+    title: 'VP Technical Services at ComputerCare',
     review:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione sequi saepe dicta blanditiis, molestias eum excepturi quod fugiat veniam!',
   },
   {
-    img: avatar3,
-    name: 'John Doe 3',
-    title: 'Colleague',
+    img: dan,
+    name: 'Dan Djalili',
+    title: 'Senior IT Support Engineer at Credit Karma',
     review:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione sequi saepe dicta blanditiis, molestias eum excepturi quod fugiat veniam!',
   },
   {
-    img: avatar4,
-    name: 'John Doe 4',
-    title: 'Colleague',
+    img: kristan,
+    name: 'Kristan Manansala',
+    title: 'Systems Engineer at ComputerCare',
     review:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione sequi saepe dicta blanditiis, molestias eum excepturi quod fugiat veniam!',
+      'Bryce was an essential member of the team. His technical expertise and experience played a huge part in the success of the project. He communicates well with end users and provides excellent technical support. His attention to detail reflects on how he writes technical notes on tickets and wiki pages. He is always willing to help and provide resources to other members of the team. He is willing to share ideas to improve our processes and workflows. I had such a great experience working with him.',
   },
 ]
